b37589645c4af258be5bdcaa79da61fad60582fe
Engie Ruge <engnatruve@gmail.com>
2019-12-10 21:19:28 -0500

    Actualizacion archivos
    Actualizacion archivos proyecto

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+(function() {
+
+	var hamburger = {
+		navToggle: document.querySelector('.nav-toggle'),
+		nav: document.getElementById('navM'),
+
+		doToggle: function(e) {
+			e.preventDefault();
+			this.navToggle.classList.toggle('expanded');
+			this.nav.classList.toggle('expanded');
+		}
+	};
+
+	hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+
+	/*Botones Links*/
+	  $('a[href^="#"]').on('click', function(event) {
+
+	    var target = $(this.getAttribute('href'));
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').stop().animate({
+	            scrollTop: target.offset().top
+	        }, 1000);
+	      }
+	    });
+
+}());
