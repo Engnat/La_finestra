e6851dc5dd59a14e5af5f0df5ad102651ded07c5
Engie Ruge <engnatruve@gmail.com>
2020-01-27 19:09:19 -0500

    Inclusion funcionamiento formularios , ajuste hojas sección finestra

---
+++ contacto.php
@@ -0,0 +1,27 @@
+<?php
+$remitente = $_POST['correoUser'];
+$destinatario = 'engnatruve@gmail.com';
+$asunto = 'Mensaje - Contacto';
+if (!$_POST){
+?>
+
+<?php
+}else{
+
+    $cuerpo = "Nombre: " . $_POST["nombreUser"] . "\r\n";
+    $cuerpo .= "Email: " . $_POST["correoUser"] . "\r\n";
+    $cuerpo .= "Telefono: " . $_POST["telefonoUser"] . "\r\n";
+  	$cuerpo .= "Asunto: " . $_POST["mensajeUser"] . "\r\n";
+
+    $headers  = "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/plain; charset=utf-8\n";
+    $headers .= "X-Priority: 3\n";
+    $headers .= "X-MSMail-Priority: Normal\n";
+    $headers .= "X-Mailer: php\n";
+    $headers .= "From: \"".$_POST['nombreUser']." \" <".$remitente.">\n";
+
+    mail($destinatario, $asunto, $cuerpo, $headers);
+
+    include 'confirmacion.html';
+}
+?>
